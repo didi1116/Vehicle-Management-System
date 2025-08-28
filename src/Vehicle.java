b29225bfaf3e4fc9dddf6b0d0af386f554430719
didi1116 <phamvy1622004@gmail.com>
2025-08-28 16:39:47 +0900

    second

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,5 +1,5 @@
 abstract class Vehicle {
-    String brend;
+    String brand;
     String model;
     Double price;
 
---
+++ src/car.java
@@ -0,0 +1,10 @@
+public class car extends Vehicle {
+    public car (String brand, String model, Double price) {
+        super (brand, model, price);
+    }
+    public void startEngine(){
+
+    }
+
+
+}
