bf1fa2c8fd21a816982500633c4c6c8ba20580cb
tksdl1216 <tksdl1216@naver.com>
2025-08-28 17:12:54 +0900

    수정

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,12 +1,17 @@
 abstract class Vehicle {
-    String brand;
-    String model;
-    Double price;
+    protected String brand;
+    protected String model;
+    protected Double price;
 
     public Vehicle(String brand, String model, Double price) {
         this.brand = brand;
         this.model = model;
         this.price = price;
+    }
+    public void displayInfo(){
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Price: " + price);
     }
 
     abstract  void startEngine();
