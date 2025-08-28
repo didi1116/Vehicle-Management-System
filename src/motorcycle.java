35fb64820b4cc2f9bb0ed4efc48b789428b2fea7
sixdayz6 <70253711+sixdayz6@users.noreply.github.com>
2025-08-28 17:18:56 +0900

    main function

--- src/motorcycle.java
+++ src/motorcycle.java
@@ -1,6 +1,6 @@
 public class motorcycle extends Vehicle {
     public motorcycle (String brand, String model, Double price) {
-
+        super (brand, model, price);
     }
     public void startEngine(){
         System.out.println("Engine is started");
--- src/truck.java
+++ src/truck.java
@@ -1,6 +1,6 @@
 public class truck extends Vehicle{
     public truck(String brand, String model, double price){
-
+        super (brand, model, price);b
     }
     public void startEngine(){
         System.out.println("Engine is started");
